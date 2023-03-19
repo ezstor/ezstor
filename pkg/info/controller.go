210a0c1692058ac6c052d8072a76bacd9779ebc4
serialq7ic4 <qianch714@gmail.com>
2023-03-19 12:19:02 +0800

    refactor: use strings package instead of shell egrep & awk tools

--- pkg/info/controller.go
+++ pkg/info/controller.go
@@ -7,17 +7,33 @@ import (
 
 func Controllers() ([]string, error) {
 	outb, err := exec.Command(
-		"/bin/sh", "-c",
-		`lspci | grep "^[0-9,a-z]" | grep -E 'Fusion-MPT|MegaRAID|Adaptec' | awk -F ':' '{print $NF}' | awk -F '[(|[]' '{print $1}' | awk '{gsub(/^\s+|\s+$/, "");print}'`,
+		"/bin/sh", "-c", `lspci`,
 	).Output()
 	if err != nil {
 		return nil, err
 	}
-	output := string(outb)
 
 	var conts []string
-	if output != "" {
-		conts = strings.Split(strings.Trim(output, "\n"), "\n")
+	for _, line := range strings.Split(strings.Trim(string(outb), "\n"), "\n") {
+		switch {
+		case strings.Contains(line, "Mass storage controller") ||
+			strings.Contains(line, "RAID bus controller") ||
+			strings.Contains(line, "Serial Attached SCSI controller"):
+			var cont string
+			elems := strings.Split(line, ":")
+			cont = elems[len(elems)-1]
+			cont = strings.Trim(cont, " ")
+			if strings.Contains(cont, "(") {
+				cont = strings.Split(cont, "(")[0]
+				cont = strings.Trim(cont, " ")
+			}
+			if strings.Contains(cont, "[") {
+				cont = strings.Split(cont, "[")[0]
+				cont = strings.Trim(cont, " ")
+			}
+			conts = append(conts, cont)
+		}
 	}
+
 	return conts, nil
 }
